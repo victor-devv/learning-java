d2f1ff965339721c44b1d51f4bc643de284b77f2
victor-devv <vheecthurikuomola@gmail.com>
2022-01-04 20:29:01 +0100

    Collect user input using Scanner

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //Using Scanner class to accept user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        //clear \n from scanner since nextInt doesn't take the \n from the enter key. Not doing this would prevent the next nextLine() input from allowing user inputs
+        scanner.nextLine();
+
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old.");
+        System.out.println("You like "+food);
+
+
+
+    }
+}
